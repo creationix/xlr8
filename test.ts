60877b4b81b773e2d551357bd98e4d39a945c4a9
Tim Caswell <tim@creationix.com>
2021-03-31 00:18:57 -0500

    Add more submodules for testin

--- test.ts
+++ test.ts
@@ -6,6 +6,8 @@ initialize(storage);
 
 const conquest = await importFolder(storage, "./conquest");
 const exploder = await importFolder(storage, "./exploder");
+const lit = await importFolder(storage, "./lit");
+const luvit = await importFolder(storage, "./luvit");
 const all = await importFolder(storage, ".");
 const cache = {};
 console.log("Iterating at conquest...");
@@ -14,6 +16,14 @@ for await (const path of iterate(storage, cache, conquest)) {
 }
 console.log("Iterating at exploder...");
 for await (const path of iterate(storage, cache, exploder)) {
+  console.log(path);
+}
+console.log("Iterating at lit...");
+for await (const path of iterate(storage, cache, lit)) {
+  console.log(path);
+}
+console.log("Iterating at luvit...");
+for await (const path of iterate(storage, cache, luvit)) {
   console.log(path);
 }
 
